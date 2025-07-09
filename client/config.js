9d71a69d1c37a36be14e6a19d5d3872fabcdf4fe
perunamuussi <perunamuussi13@proton.me>
2025-07-09 14:58:34 +0300

    changed file location

---
+++ client/config.js
@@ -0,0 +1,6 @@
+const config = {
+  PUBLIC_API_URL: 'https://quoteslate.vercel.app',
+  LOCAL_API_URL: 'https://random-quotes-oop-28ec.onrender.com',
+};
+
+export default config;
--- client/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  PUBLIC_API_URL: 'https://quoteslate.vercel.app',
-  LOCAL_API_URL: 'http://localhost:3000',
-};
-
-export { config };
